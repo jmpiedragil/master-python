d00466fe8343d763aaba13b4a2673c14158100bf
Javier Piedragil <jpiedragil@uttab.edu.mx>
2020-04-25 00:56:33 -0500

    Lección 8
    Primer ejemplo del uso de variables.

---
+++ 02-variables-y-tipos/variables.py
@@ -0,0 +1,12 @@
+"""
+
+Una variable es un contenedor de información
+que dentro guardará un dato, se pueden crear
+muchas variables y que cada una tenga un dato distinto.
+
+"""
+
+texto = "Máster en Python"
+texto = 1
+
+print(texto)
