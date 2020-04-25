353246d93dd173b9dc8cd71f7df51e67afa4e126
Javier Piedragil <jpiedragil@uttab.edu.mx>
2020-04-25 01:20:02 -0500

    Lección 9
    - Ejemplos de diferentes tipos de concatenación y formateo de
     cadenas de caracteres.

--- 02-variables-y-tipos/variables.py
+++ 02-variables-y-tipos/variables.py
@@ -6,11 +6,13 @@ muchas variables y que cada una tenga un dato distinto.
 
 """
 
+# Crear variables y asignarles un valor
 texto = "Máster en Python"
 texto2 = "con Víctor Robles"
 numero = 45
 decimal = 6.7
 
+# Mostrar el valor de las variables
 print(texto)
 print(texto2)
 print(numero)
@@ -18,8 +20,23 @@ print(decimal)
 
 print("---------------------------------------------------------")
 
+# Sustituir el valor de algunas variables / reasignado valores
 numero = 77
 decimal = 8.9
 
 print(numero)
 print(decimal)
+
+print("---------------------------------------------------------")
+
+# Concatenación
+nombre = "Víctor"
+apellidos = "Robles"
+web = "victorroblesweb.es"
+
+print(nombre + " " + apellidos + " - " + web)
+print(f"{nombre} {apellidos} - {web}")
+print("Hola me llamo {} {} y mi web es {}".format(nombre, apellidos, web))
+
+# No es concatenación
+print(nombre, apellidos, web)
