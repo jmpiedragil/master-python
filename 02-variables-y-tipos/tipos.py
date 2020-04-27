77541c7395e233361db76007ff0beff4c8b6c309
Javier Piedragil <jpiedragil@uttab.edu.mx>
2020-04-27 15:54:31 -0500

    Lección 11
    - Ejemplo que muestra la conversión de datos de un tipo a otro.

--- 02-variables-y-tipos/tipos.py
+++ 02-variables-y-tipos/tipos.py
@@ -20,3 +20,15 @@ print(dato_byte)
 
 # Mostrar tipo de dato
 print(type(dato_byte))
+
+texto = "Hola soy un texto"
+numerito = str(776)
+print(type(numerito))
+
+print(texto + " " + numerito)
+
+numerito = int(776)
+print(type(numerito))
+
+numerito = float(776)
+print(type(numerito))
