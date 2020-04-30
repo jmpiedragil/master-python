e204013415e81825f9c8cd8dc95508af58dcb61d
Javier Piedragil <jpiedragil@uttab.edu.mx>
2020-04-30 02:05:24 -0500

    Lección 15
    - Ejemplo que muestra una simulación rn Pyuthon de los operadores de
     incremento y decremento que existen en otros lenguajes.

--- 03-operadores/asignacion.py
+++ 03-operadores/asignacion.py
@@ -8,3 +8,14 @@ edad += 5
 edad = edad + 5
 edad -= 5
 print(edad)
+
+# Operadores incremento y decremento
+year = 2021
+
+# Incremento
+year = year + 1
+
+# Decremento
+year = year - 1
+
+print(year)
