b164e04b1ccc78ad3179abbfdbf83f5f509bcbe0
Javier Piedragil <jpiedragil@uttab.edu.mx>
2020-04-30 01:44:00 -0500

    Lección 13
    - Primer ejemplo del uso de operadores aritméticos.

---
+++ 03-operadores/aritmeticos.py
@@ -0,0 +1,8 @@
+# Operadores artiméticos
+numero1 = 77
+numero2 = 44 # operador de asignación =
+
+resta = numero1 - numero2
+
+print("********** CALCULADORA **********")
+print(f"La resta es {resta}")
