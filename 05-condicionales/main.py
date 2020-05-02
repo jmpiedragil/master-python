59a1ff1f112ef7235e6166f7fe1e856b7f4cb365
Javier Piedragil <jpiedragil@uttab.edu.mx>
2020-05-02 00:07:24 -0500

    Lección 18
    - Ejemplo del uso de estructuras condicionales.

---
+++ 05-condicionales/main.py
@@ -0,0 +1,26 @@
+"""
+# Condicional IF
+
+SI se_cumple_esta_condición:
+  Ejecutar grupo de instrucciones
+SI NO:
+  Se ejecutan otro grupo de instrcciones
+
+if condicion:
+  instrucciones
+else:
+  otras instrucciones
+
+"""
+
+# Ejemplo 1
+
+print("########## EJEMPLO 1 ##########")
+
+color = "rojo"
+
+if color == "rojo":
+  print("¡¡Enhorabuena!!")
+  print("El color es ROJO")
+else:
+  print("El color NO es ROJO")
