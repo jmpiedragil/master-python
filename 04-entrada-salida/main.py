d8278a3c4a2b0a33f9ad13738eeb810691d7ce1c
Javier Piedragil <jpiedragil@uttab.edu.mx>
2020-05-01 23:53:03 -0500

    Lección 17
    - Ejemplo que muestra la entrada y salida de datos.

---
+++ 04-entrada-salida/main.py
@@ -0,0 +1,6 @@
+# Entrada
+nombre = input("¿Cúal es tu nombre?: ")
+edad = input("¿Cuál es tu edad?: ")
+
+# Salida
+print(f"¡¡Me alegro de conocerte, bienvenido {nombre}, veo que tienes {int(edad) + 2} años!!")
