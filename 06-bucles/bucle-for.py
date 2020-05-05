43d53f2ad504666e203c2e855b5f9bf9330b17f3
Javier Piedragil <jpiedragil@uttab.edu.mx>
2020-05-05 01:59:00 -0500

    Lección 24
    - Variación simple del eejemplo anterior.

--- 06-bucles/bucle-for.py
+++ 06-bucles/bucle-for.py
@@ -7,6 +7,11 @@ for variable in elemento iterable (lista, rango, etc.)
 """
 
 contador = 0
+resultado = 0
 
 for contador in range(0,10):
   print("Voy por el " + str(contador))
+
+  resultado += contador
+
+print(f"El resultado es: {resultado}")
