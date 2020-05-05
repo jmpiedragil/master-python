33389dff053639861dab909dd2a61a05e42e6cd1
Javier Piedragil <jpiedragil@uttab.edu.mx>
2020-05-05 01:52:24 -0500

    Lección 24
    - Ejemplo simple que muestra el uso de la estructura de control for.

---
+++ 06-bucles/bucle-for.py
@@ -0,0 +1,12 @@
+"""
+
+# FOR
+for variable in elemento iterable (lista, rango, etc.)
+  BLOQUE DE INSTRUCCIONES
+
+"""
+
+contador = 0
+
+for contador in range(0,5):
+  print("Voy por el " + str(contador))
