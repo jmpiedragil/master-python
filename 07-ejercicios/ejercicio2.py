a7dc13e44a6dc9a6beb19c5a3c6a8d3e9c99e081
Javier Piedragil <jpiedragil@uttab.edu.mx>
2020-05-08 04:28:55 -0500

    Lección 29
    - Ejercicio 2: Imprimir una lista de números pares, versión 1.

---
+++ 07-ejercicios/ejercicio2.py
@@ -0,0 +1,14 @@
+"""
+Ejercicio 2
+
+  - Escribir un script que nos muestre por pantalla
+    todos lo números pares del 1 al 100
+
+ """
+
+contador = 1
+
+for contador in range(1, 121):
+  if contador % 2 == 0:
+    print(contador)
+print("Lista de números pares terminada.")
