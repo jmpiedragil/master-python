2f61dea5c7e0c8f7eb60c39c540b5397f7097319
Javier Piedragil <jpiedragil@uttab.edu.mx>
2020-05-08 04:58:22 -0500

    Lección 31
    - Ejercicio 4: Codificar una calculadora básica que sume, reste,
     multiplique y divida dos números.

---
+++ 07-ejercicios/ejercicio4.py
@@ -0,0 +1,15 @@
+"""
+Ejercicio 4
+  - Pedir dos números al usuario y hacer todas las
+    operaciones básicas de una calculadora y mostrarlo por pantalla
+"""
+
+numero1 = int(input("Por favor introducza el primer número: "))
+numero2 = int(input("Por favor introducza el segundo número: "))
+
+print("####### CALCULADORA #######")
+
+print(f"{numero1} + {numero2} = {numero1 + numero2}")
+print(f"{numero1} - {numero2} = {numero1 - numero2}")
+print(f"{numero1} * {numero2} = {numero1 * numero2}")
+print(f"{numero1} / {numero2} = {numero1 / numero2}")
