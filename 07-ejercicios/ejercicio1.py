c6565e43296d3871cd05ad83cd4e3cc171c84707
Javier Piedragil <jpiedragil@uttab.edu.mx>
2020-05-08 04:19:14 -0500

    Lección 28
    - Ejercicio número 1; declaración, impresión de variables y uso
    de cometarios.

---
+++ 07-ejercicios/ejercicio1.py
@@ -0,0 +1,19 @@
+"""
+Ejercicio 1
+  - Crear variables "pais" y otra "continente".
+  - Mostrar su valor por pantalla (imprimir).
+  - Poner un comentario diciendo el tipo de dato
+
+"""
+
+pais = "Mexico"
+continente = "América"
+
+# Tipo de dato: Cadena de caracteres
+print(pais)
+# Tipo de dato: Cadena de caracteres
+print(continente)
+
+year = 2020 # int
+
+print(f"{pais} - {continente} - {str(year)}")
