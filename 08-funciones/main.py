54ec29b71ca25ee191d003ffca1347660388a08a
Javier Piedragil <jpiedragil@uttab.edu.mx>
2020-05-09 00:15:10 -0500

    Lección 38
    - Ejemplo de creación y ejecución de una función.

---
+++ 08-funciones/main.py
@@ -0,0 +1,32 @@
+"""
+
+Funciones:
+
+Una función es un conjunto de instrucciones agrupadas bajo un nombre
+concreto que pueden reutilizarse invocando a la función tantas veces
+como sea necesario.
+
+def nombreDeMiFuncion(parámetros):
+  # Bloque / conjunto de instrucciones
+
+nombreDeMiFuncion(mi_parametro)
+
+"""
+# Ejemplo 1
+
+print("##### EJEMPLO 1 #####")
+
+# Definir función
+def muestraNombre():
+  print("Víctor")
+  print("Paco")
+  print("Juan")
+  print("Francisco")
+  print("Aitor")
+  print("Néstor")
+  print("\n")
+
+# Invocar función
+muestraNombre()
+muestraNombre()
+muestraNombre()
