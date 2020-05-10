fb466373e2b710898e78ccfe9286076ba41a4067
Javier Piedragil <jpiedragil@uttab.edu.mx>
2020-05-10 02:43:49 -0500

    Lección 45
    - Variante del ejemplo anterior que resalta el comportamiento de las
     variables globales.

--- 08-funciones/variable.py
+++ 08-funciones/variable.py
@@ -14,7 +14,7 @@ frase = "Ni los genios son tan genios, ni los mediocres tan mediocres"
 print(frase)
 
 def holaMundo():
-  frase = "¡¡Hola mundo!!"
+  # frase = "¡¡Hola mundo!!"
   print(frase)
 
 holaMundo()
