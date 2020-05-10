9183f355a3f5b2924804834523d99c004fb8b046
Javier Piedragil <jpiedragil@uttab.edu.mx>
2020-05-10 03:58:38 -0500

    Lección 46
    - Se añaden ejemplos de diferentes funciones predefinidas.

--- 08-funciones/predefinidas.py
+++ 08-funciones/predefinidas.py
@@ -24,4 +24,25 @@ year = 2022
 print(year)
 
 del year
-print(year)
+# print(year)
+
+# Comprobar variable vacía
+texto = " ff  "
+
+if len(texto) < 0:
+  print("La variable está vacía")
+else:
+  print("La variable tiene contenido: ", len(texto))
+
+# Encontrar caracteres
+frase = "La vida es bella"
+print(frase.find("vida"))
+
+# Reemplazar pálabras en un string
+nueva_frase = frase.replace("vida", "moto")
+print(nueva_frase)
+
+# Mayúsculas y minúsculas
+print(nombre)
+print(nombre.lower())
+print(nombre.upper())
