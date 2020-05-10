401529e8275ad59de090dd3c8c4bea8a6c69791b
Javier Piedragil <jpiedragil@uttab.edu.mx>
2020-05-10 04:06:05 -0500

    Lección 47
    - Ejemplo que muestra recomendaciones y algunas particularidades de las
    funciones en Python.

---
+++ 08-funciones/curiosidades.py
@@ -0,0 +1,15 @@
+def miFuncion():
+  print("Hola qué tal")
+
+def mi_segunda_funcion():
+  print("Hola qué tal 2")
+
+
+nombre = "Javier Melquisedec"
+apellidos = "Piedragil Gálvez"
+
+print("Hola mundo")
+print(f"Bienvenido {nombre}")
+
+miFuncion()
+mi_segunda_funcion()
