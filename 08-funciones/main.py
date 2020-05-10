6f09d9d9f538c3dad8c45523a3e63143faedae02
Javier Piedragil <jpiedragil@uttab.edu.mx>
2020-05-09 22:55:27 -0500

    Lección 42
    - Ejemplo que muestra el uso de parámetros opcionales y regreso
    de valores en una función.

--- 08-funciones/main.py
+++ 08-funciones/main.py
@@ -91,3 +91,27 @@ def saludame(nombre):
   return saludos
 
 print(saludame("Javier"))
+
+# Ejemplo 6
+print("##### EJEMPLO 6 #####")
+
+def calculadora(numero1, numero2, basicas = False):
+
+  suma = numero1 + numero2
+  resta = numero1 - numero2
+  multi = numero1 * numero2
+  division = numero1 / numero2
+
+  cadena = ""
+
+  cadena += "Suma: " + str(suma)
+  cadena += "\n"
+  cadena += "Resta: " + str(resta)
+  cadena += "\n"
+  cadena += "Multiplicación: " + str(multi)
+  cadena += "\n"
+  cadena += "División: " + str(division)
+
+  return cadena
+
+print(calculadora(5, 5))
