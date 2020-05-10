02190b42dfd2e3bb76370f34d4cdeba5f72ed980
Javier Piedragil <jpiedragil@uttab.edu.mx>
2020-05-10 04:11:34 -0500

    Lección 47
    - Variante simple del ejemplo anterior.

--- 08-funciones/curiosidades.py
+++ 08-funciones/curiosidades.py
@@ -1,8 +1,8 @@
 def miFuncion():
-  return "Hola qué tal"
+  return "Hola qué tal " + nombre
 
 def mi_segunda_funcion():
-  return "Hola qué tal 2"
+  return "Hola qué tal 2 " + apellidos
 
 
 nombre = "Javier Melquisedec"
