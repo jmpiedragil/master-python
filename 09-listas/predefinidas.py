10b86707f4623fad49095509e7f6c1001b99b537
Javier Piedragil <jpiedragil@uttab.edu.mx>
2020-05-11 23:40:03 -0500

    Lección 53
    - Ejemplo que muestra diferentes funciones predefinidas que
    actúan sobre las listas.

---
+++ 09-listas/predefinidas.py
@@ -0,0 +1,45 @@
+cantantes = ['2pac', 'Drake', 'Bad Bunny', 'Julio Iglesias']
+numeros = [1, 2, 5, 8, 3, 4]
+
+# Ordenar
+print(numeros)
+numeros.sort()
+print(numeros)
+
+# Añadir elementos
+cantantes.append("Natos y Waor")
+cantantes.insert(1, "David Bisbal")
+
+print(cantantes)
+
+# Eliminar elementos
+cantantes.pop(1)
+cantantes.remove("Bad Bunny")
+
+print(cantantes)
+
+# Dar la vuelta
+print(numeros)
+numeros.reverse()
+print(numeros)
+
+# Buscar dentro de una lista
+print( 'Drake' in cantantes)
+
+# Contar elementos
+print(cantantes)
+print(len(cantantes))
+
+# Cuántas veces aparece un elemento
+print(numeros)
+numeros.append(8)
+print(numeros)
+print(numeros.count(8))
+
+# Conseguir índice
+print(cantantes)
+print(cantantes.index("Drake"))
+
+# Unir listas
+cantantes.extend(numeros)
+print(cantantes)
