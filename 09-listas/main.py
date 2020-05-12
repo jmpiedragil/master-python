4cbbf53a155cc749c144834d7bf86b767524be61
Javier Piedragil <jpiedragil@uttab.edu.mx>
2020-05-11 21:34:34 -0500

    Lección 48
    - Ejemplo que muestra la creación de listas (arreglos).

---
+++ 09-listas/main.py
@@ -0,0 +1,21 @@
+"""
+
+LISTAS (arrays)
+
+  - Son colecciones o conjuntos de datos / valores, bajo un único nombre.
+  - Para acceder a esos valores podemos usar un índice numérico.
+
+"""
+
+pelicula = "Batman"
+
+# Definir Lista
+peliculas = ["Batman", "Spiderman", "El señor de los anillos"]
+cantantes = list(('2pac', 'Drake', 'Jennifer López'))
+years = list(range(2020, 2050))
+variada = ["Javier", 30, 4.4, True, "Texto"]
+
+print(peliculas)
+print(cantantes)
+print(years)
+print(variada)
