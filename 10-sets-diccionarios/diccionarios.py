291b2d40074e6f22421e382fda851b8b13edb4a8
Javier Piedragil <jpiedragil@uttab.edu.mx>
2020-05-13 22:46:29 -0500

    👌 IMPROVE:  Lección 56
    - Variación del ejemplo anterior que muestra el uso de
    diccionarios dentro de una lista.

--- 10-sets-diccionarios/diccionarios.py
+++ 10-sets-diccionarios/diccionarios.py
@@ -19,3 +19,34 @@ print(persona)
 
 print(persona["apellidos"])
 print(persona["web"])
+
+# Lista con diccionarios
+
+contactos = [
+  {
+    'nombre': 'Antonio',
+    'email': 'antonio@antonio.com'
+  },
+  {
+    'nombre': 'Luis',
+    'email': 'luis@luis.com'
+  },
+  {
+    'nombre': 'Salvador',
+    'email': 'salvador@salvador.com'
+  }
+]
+
+print(contactos)
+
+contactos[0]["nombre"] = "Antoñito"
+
+print(contactos[0]["nombre"])
+
+print("\nListado de contactos")
+print("----------------------------------------")
+
+for contacto in contactos:
+  print(f"Nombre del contacto: {contacto['nombre']}")
+  print(f"Email del contacto: {contacto['email']}")
+  print("----------------------------------------")
