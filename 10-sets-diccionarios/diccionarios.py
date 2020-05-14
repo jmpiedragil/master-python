032056b823b7a17201f3be9c602db904d15582a7
Javier Piedragil <jpiedragil@uttab.edu.mx>
2020-05-13 22:28:12 -0500

    📦 NEW: Lección 55
    - Ejemplo que muestra el uso del tipo de dato diccionario.

---
+++ 10-sets-diccionarios/diccionarios.py
@@ -0,0 +1,21 @@
+"""
+
+Diccionario:
+  Un tipo de dato que alamacena un conjunto de datos.
+  En formato clave > valor.
+  Es parecido a un array asociativo o un objeto JSON.
+
+"""
+
+persona = {
+  "nombre": "Javier",
+  "apellidos": "Piedragil Gálvez",
+  "web": "piedragil.com"
+
+}
+
+print(type(persona))
+print(persona)
+
+print(persona["apellidos"])
+print(persona["web"])
