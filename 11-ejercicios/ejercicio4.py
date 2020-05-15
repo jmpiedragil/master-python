8d4e795eab0512a5e7559699ab23c46a84d4fe58
Javier Piedragil <jpiedragil@uttab.edu.mx>
2020-05-14 23:56:22 -0500

    👌 IMPROVE: Lección 61
    - Variante del ejemplo anterior.

--- 11-ejercicios/ejercicio4.py
+++ 11-ejercicios/ejercicio4.py
@@ -6,16 +6,42 @@ Ejercicio 4
   Usar funciones
 
 """
-def definirTipo(variable):
 
-  return type(variable)
+def traducirTipo(tipo):
+
+  resultado = ""
+
+  if tipo == list:
+    resultado = "lista"
+  elif tipo == str:
+    resultado = "cadena de texto"
+  elif tipo == int:
+    resultado = "entero"
+  elif tipo == bool:
+    resultado = "booleano"
+  else:
+    resultado = "desconocido"
+
+  return resultado
+
+
+def definirTipo(variable, tipo):
+
+  resultado = ""
+
+  if isinstance(variable, tipo):
+    resultado = f"Esta variable es de tipo {traducirTipo(type(variable))}"
+  else:
+    resultado = f"Esta variable no es de tipo {traducirTipo(tipo)}, es {traducirTipo(type(variable))}"
+
+  return resultado
 
 lista = [1, 2, 3, 4, 5]
 cadena_caracteres = "Soy un string"
 numero = 26
 booleano = True
 
-print(definirTipo(lista))
-print(definirTipo(cadena_caracteres))
-print(definirTipo(numero))
-print(definirTipo(booleano))
+print(definirTipo(lista, list))
+print(definirTipo(cadena_caracteres, str))
+print(definirTipo(numero, int))
+print(definirTipo(booleano, bool))
