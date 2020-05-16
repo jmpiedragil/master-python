6771ceb0bea84d7007bff5a960e093f2bd323a20
Javier Piedragil <jpiedragil@uttab.edu.mx>
2020-05-15 23:11:46 -0500

    👌 IMPROVE: Lección 64
    - Ejemplo que muestra la creación y uso de un módulo.

--- 12-modulos/main.py
+++ 12-modulos/main.py
@@ -9,4 +9,8 @@ Podemos conseguir módulos que ya vienen en el lenguaje, módulos en internet y
 también podemos crear nuestros módulos.
 
 """
+import mimodulo
 
+print(mimodulo.holaMundo("Javier Piedragil Gálvez"))
+
+print(mimodulo.calculadora(3, 5, True))
