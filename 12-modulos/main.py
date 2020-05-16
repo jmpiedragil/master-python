f9e5e5fdbc6e8cd3ecc07bfa1797db6093abb40b
Javier Piedragil <jpiedragil@uttab.edu.mx>
2020-05-15 23:20:09 -0500

    👌 IMPROVE: Lección 65
    - Variante simple del ejemplo anterior.

--- 12-modulos/main.py
+++ 12-modulos/main.py
@@ -9,10 +9,13 @@ Podemos conseguir módulos que ya vienen en el lenguaje, módulos en internet y
 también podemos crear nuestros módulos.
 
 """
-from mimodulo import holaMundo
+#from mimodulo import holaMundo
+from mimodulo import *
 
 # print(mimodulo.holaMundo("Javier Piedragil Gálvez"))
 
 # print(mimodulo.calculadora(3, 5, True))
 
 print(holaMundo("Javier Piedragil Gálvez"))
+
+print(calculadora(3, 5, True))
