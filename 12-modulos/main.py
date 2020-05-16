b7ca3d729763d8acfbb5675f63a488dd7830ead5
Javier Piedragil <jpiedragil@uttab.edu.mx>
2020-05-15 23:01:37 -0500

    📦 NEW: Lección 63
    - Encabezado del  archivo que contiene los ejemplos.

---
+++ 12-modulos/main.py
@@ -0,0 +1,12 @@
+"""
+
+Módulos: Son funcionalidades ya hechas para reutilizar.
+
+En Python hay muchos módulos, que los puedes consultar aquí:
+https://docs.python.org/3/py-modindex.html
+
+Podemos conseguir módulos que ya vienen en el lenguaje, módulos en internet y
+también podemos crear nuestros módulos.
+
+"""
+
