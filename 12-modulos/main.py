b3e625930162602f963f3c0c13e877cae629a715
Javier Piedragil <jpiedragil@uttab.edu.mx>
2020-05-15 23:16:37 -0500

    👌 IMPROVE: Lección 64
    - Variante simple del ejemplo anterior; se muestra como importar selectivamente a las
    funciones dentro de un módulo.

--- 12-modulos/main.py
+++ 12-modulos/main.py
@@ -9,8 +9,10 @@ Podemos conseguir módulos que ya vienen en el lenguaje, módulos en internet y
 también podemos crear nuestros módulos.
 
 """
-import mimodulo
+from mimodulo import holaMundo
 
-print(mimodulo.holaMundo("Javier Piedragil Gálvez"))
+# print(mimodulo.holaMundo("Javier Piedragil Gálvez"))
 
-print(mimodulo.calculadora(3, 5, True))
+# print(mimodulo.calculadora(3, 5, True))
+
+print(holaMundo("Javier Piedragil Gálvez"))
