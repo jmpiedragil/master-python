42f6e6293a80fe01a7c4f20ebb1d47d949085d97
Javier Piedragil <jpiedragil@uttab.edu.mx>
2020-05-17 22:27:20 -0500

    📦 NEW: Lección 69
    - Ejemplo que muestra como abrir, escribir y cerrar un archivo en
    el sistema de archivos.

--- 14-sistema-archivos/ficheros.py
+++ 14-sistema-archivos/ficheros.py
@@ -4,4 +4,13 @@ import pathlib
 # Abrir archivo
 ruta = str(pathlib.Path().absolute()) + "/fichero_texto.txt"
 
-print(ruta)
+archivo = open(ruta, "a+")
+
+# Escribir
+
+archivo.write("********** Soy un texto metido desde Python **********\n")
+
+# Cerrar archivo
+
+archivo.close()
+
