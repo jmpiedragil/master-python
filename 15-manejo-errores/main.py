12f4736033f7b6c97baec451ebbde1d266f31fb2
Javier Piedragil <jpiedragil@uttab.edu.mx>
2020-05-19 03:32:37 -0500

    📦 NEW: Lección 77
    - Ejemplo que muestra el uso de múltiples excepciones.

--- 15-manejo-errores/main.py
+++ 15-manejo-errores/main.py
@@ -1,6 +1,7 @@
 # Capturar excepciones y manejar errores en código
 # susceptible a fallas / errores.
 
+"""
 try:
   nombre = input("¿Cuál es tu nombre: ")
 
@@ -14,3 +15,16 @@ else:
   print("Todo ha funcionado correctamente")
 finally:
   print("¡¡Fin de la iteración!!")
+"""
+# Múltiples excepciones
+
+try:
+  numero = int(input("Número para elevarlo al cuadarado: "))
+
+  print("El cuadrado es " + str(numero * numero))
+except TypeError:
+  print("¡¡Debes convertir tus cadenas a enteros!!")
+except ValueError:
+  print("¡¡Introduce un número correcto!!")
+except Exception as e:
+  print("Ha ocurrido un error:", type(e)-__name__)
