27b0a854a0a35738164c6030abc1863b1d0ace5e
Javier Piedragil <jpiedragil@uttab.edu.mx>
2020-05-19 03:54:10 -0500

    📦 NEW: Lección 58
    - Ejemplo que muestra el uso de excepciones personalizadas.

--- 15-manejo-errores/main.py
+++ 15-manejo-errores/main.py
@@ -18,6 +18,7 @@ finally:
 """
 # Múltiples excepciones
 
+"""
 try:
   numero = int(input("Número para elevarlo al cuadarado: "))
 
@@ -29,3 +30,21 @@ except TypeError:
 except Exception as e:
   print(type(e))
   print("Ha ocurrido un error:", type(e).__name__)
+"""
+
+# Excepciones "perwonalizadas" o lanzar excepción
+
+try:
+  nombre = input("introduce el nombre: ")
+  edad = int(input("Introduce la edad: "))
+
+  if edad < 5 or edad > 110:
+    raise ValueError("La edad introducida no es real")
+  elif len(nombre) <= 1:
+    raise ValueError("El nombre no está completo")
+  else:
+    print(f"¡¡Bienvenido al Master en Python {nombre}!!")
+except ValueError:
+  print("Introduce los datos correctamente")
+except Exception as e:
+  print("Existe un error: ", e)
