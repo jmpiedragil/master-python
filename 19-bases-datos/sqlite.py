824c044ef92ddd3114ee70abbc283d7d8a5ddf40
Javier Piedragil <jpiedragil@uttab.edu.mx>
2020-05-25 04:36:56 -0500

    📦 NEW: Lección 91
    - Se complementa el ejemplo para crear una bd slite.

--- 19-bases-datos/sqlite.py
+++ 19-bases-datos/sqlite.py
@@ -6,6 +6,23 @@ import sqlite3
 
 conexion = sqlite3.connect('pruebas.db')
 
+# Crear cursor
+
+cursor = conexion.cursor()
+
+# Crear tabla
+
+cursor.execute("CREATE TABLE IF NOT EXISTS producto (" +
+               "id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, " +
+               "titulo VARCHAR(255), " +
+               "descripcion TEXT, " +
+               "precio INT(10))"
+               )
+
+# Guardar cambios
+
+conexion.commit()
+
 # Cerrar conexión
 
 conexion.close()
