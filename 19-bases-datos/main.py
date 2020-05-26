cd5a5dc49deb8d7de74f1072cad97282e3208ac2
Javier Piedragil <jpiedragil@uttab.edu.mx>
2020-05-26 16:02:25 -0500

    👌 IMPROVE: Lección 97
    - Variante simple del ejemplo.

--- 19-bases-datos/main.py
+++ 19-bases-datos/main.py
@@ -5,10 +5,19 @@ import mysql.connector
 database = mysql.connector.connect(
     host="localhost",
     user="root",
-    passwd="cynara020800"
-    # database="master_python"
+    passwd="cynara020800",
+    database="master_python"
 )
 
 # ¿La conexión ha sido correcta?
 
-print(database)
+# print(database)
+
+cursor = database.cursor()
+
+cursor.execute("CREATE DATABASE IF NOT EXISTS master_python")
+
+cursor.execute("SHOW DATABASES")
+
+for bd in cursor:
+    print(bd)
