c7dca9eda2e65e2164b6d1363c94e2406167c415
Javier Piedragil <jpiedragil@uttab.edu.mx>
2020-05-27 12:08:49 -0500

    📦 NEW: Lección 98
    - Ejemplo que muestra cómo crear una tabla en MySQL.

--- 19-bases-datos/main.py
+++ 19-bases-datos/main.py
@@ -13,11 +13,28 @@ database = mysql.connector.connect(
 
 # print(database)
 
+# Cursor
+
 cursor = database.cursor()
 
+# Crear base de datos
+"""
 cursor.execute("CREATE DATABASE IF NOT EXISTS master_python")
 
 cursor.execute("SHOW DATABASES")
 
 for bd in cursor:
     print(bd)
+"""
+
+# Crear tablas
+
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS vehiculo(
+        id INT(10) AUTO_INCREMENT NOT NULL,
+        marca VARCHAR(40) NOT NULL,
+        modelo VARCHAR(40) NOT NULL,
+        precio FLOAT(10, 2) NOT NULL,
+        CONSTRAINT pk_vehiculo PRIMARY KEY(id)
+    )
+""")
