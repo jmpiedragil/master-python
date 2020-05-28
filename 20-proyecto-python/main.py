9cec2dba15bc2128bbb990a0f27662c3c602fac4
Javier Piedragil <jpiedragil@uttab.edu.mx>
2020-05-28 07:26:46 -0500

    📦 NEW: Lección 103
    - Se escibe la descripción del proyecto.

---
+++ 20-proyecto-python/main.py
@@ -0,0 +1,11 @@
+"""
+
+Proyecto Python y MySQL:
+
+- Abrir asistente
+- Login o registro
+- Si elegimos registro, crear usuario en la BD
+- Si elegimos login, identifica al usuario y nos preguntará
+- Creear nota, mostrar notas, borrarlas
+
+"""
