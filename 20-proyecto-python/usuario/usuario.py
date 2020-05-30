d68ea635436f3448cc5739979cbbaa8f0779980c
Javier Piedragil <jpiedragil@uttab.edu.mx>
2020-05-30 12:04:53 -0500

    📦 NEW: Lección 112
    - Añadir procedimiento para guardar encriptadas las contraseñas
    en el registro de usuario.

--- 20-proyecto-python/usuario/usuario.py
+++ 20-proyecto-python/usuario/usuario.py
@@ -1,5 +1,6 @@
 import mysql.connector
 import datetime
+import hashlib
 
 
 database = mysql.connector.connect(
@@ -25,9 +26,15 @@ class Usuario:
     def registrar(self):
 
         fecha = datetime.datetime.now()
+
+        # Cifrar contraseña
+
+        cifrado = hashlib.sha256()
+        cifrado.update(self.password.encode('utf8'))
+
         sql = "INSERT INTO usuario VALUES(null, %s, %s, %s, %s, %s)"
-        usuario = (self.nombre, self.apellidos, self.email, self.password,
-                   fecha)
+        usuario = (self.nombre, self.apellidos, self.email,
+                   cifrado.hexdigest(), fecha)
 
         try:
             cursor.execute(sql, usuario)
