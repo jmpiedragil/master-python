9fa554f9141bf1b1b4559143df4d48410e6d4aed
Javier Piedragil <jpiedragil@uttab.edu.mx>
2020-05-30 13:45:26 -0500

    👌 IMPROVE: Lección 113
    - Se incluye manejo de excepciones  en la clase de conexión a la
    base de datos.

--- 20-proyecto-python/usuario/conexion.py
+++ 20-proyecto-python/usuario/conexion.py
@@ -1,16 +1,26 @@
 import mysql.connector
+from mysql.connector import Error
 
 
 def conectarBd():
 
-    database = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        passwd="cynara020800",
-        database="master_python",
-        port=3306
-    )
+    try:
 
-    cursor = database.cursor(buffered=True)
+        conexionDb = mysql.connector.connect(
+                   host="localhost",
+                   user="root",
+                   passwd="cynara020800",
+                   database="master_python",
+                   port=3306
+        )
 
-    return [database, cursor]
+        if conexionDb.is_connected():
+            cursor = conexionDb.cursor(buffered=True)
+
+    except Error as e:
+
+        print("Error al conectarse a la base de datos.", e)
+        conexionDb = None
+        cursor = None
+
+    return [conexionDb, cursor]
