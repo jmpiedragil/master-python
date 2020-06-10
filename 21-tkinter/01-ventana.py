136b62d70d05227705be8ceabdb85a27e84a60f2
Javier Piedragil <jpiedragil@uttab.edu.mx>
2020-06-10 02:25:15 -0500

    📦 NEW: Lección 121
    - Crear una ventana y bloquear la funcionalidad de cambio de tamaño.

---
+++ 21-tkinter/01-ventana.py
@@ -0,0 +1,21 @@
+# Tkinter
+# Módulo para crear interfaces gráficas de usuario
+
+from tkinter import *
+
+
+# Crear la ventana raíz
+
+ventana = Tk()
+
+# Cambio en el tañamaño de la ventana
+
+ventana.geometry("750x450")
+
+# Bloquear el tamaño de la ventana
+
+ventana.resizable(0, 0)
+
+# Arrancar y mostrar la ventana hasta que se cierre.
+
+ventana.mainloop()
