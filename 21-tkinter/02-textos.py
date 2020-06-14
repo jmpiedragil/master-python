45ea8da2208879fc1be18c7cce22775058d72f0d
Javier Piedragil <jpiedragil@uttab.edu.mx>
2020-06-14 17:16:12 -0500

    👌 IMPROVE: Lección 126
    - Cambia posición del texto.
    - Cambia tipografía.

--- 21-tkinter/02-textos.py
+++ 21-tkinter/02-textos.py
@@ -5,10 +5,11 @@ ventana = tk.Tk()
 ventana.geometry("700x500")
 
 texto = tk.Label(ventana, text="Bienvenido a mi programa")
-texto.config(fg="white", bg="#000000", padx=50, pady=20, font=("Arial", 30))
+texto.config(fg="white", bg="#000000", padx=50, pady=20, font=("Consolas", 30))
 texto.pack()
 
 texto = tk.Label(ventana, text="Soy Javier M. Piedragil")
-texto.pack()
+texto.config(justify="right")
+texto.pack(anchor="e")
 
 ventana.mainloop()
