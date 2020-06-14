221c89e90d2c7e6e3ea73ea07a49ec7901e427c9
Javier Piedragil <jpiedragil@uttab.edu.mx>
2020-06-14 16:54:16 -0500

    👌 IMPROVE: Lección 126
    - Agrega configuración de la tipografía del texto
    - Cambia tamaño de la ventana.
    - Ajusta el margent interior horizontal.

--- 21-tkinter/02-textos.py
+++ 21-tkinter/02-textos.py
@@ -2,10 +2,10 @@ import tkinter as tk
 
 
 ventana = tk.Tk()
-ventana.geometry("500x500")
+ventana.geometry("700x500")
 
 texto = tk.Label(ventana, text="Bienvenido a mi programa")
-texto.config(fg="white", bg="#000000", padx=200, pady=20)
+texto.config(fg="white", bg="#000000", padx=50, pady=20, font=("Arial", 30))
 texto.pack()
 
 texto = tk.Label(ventana, text="Soy Javier M. Piedragil")
