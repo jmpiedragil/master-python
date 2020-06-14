5d43f077c9c8cd91e7cc77d30e0179cc8f1d3f04
Javier Piedragil <jpiedragil@uttab.edu.mx>
2020-06-14 17:16:40 -0500

    👌 IMPROVE: Lección 126
    - Cambia posición del texto.

--- 21-tkinter/02-textos.py
+++ 21-tkinter/02-textos.py
@@ -9,7 +9,6 @@ texto.config(fg="white", bg="#000000", padx=50, pady=20, font=("Consolas", 30))
 texto.pack()
 
 texto = tk.Label(ventana, text="Soy Javier M. Piedragil")
-texto.config(justify="right")
-texto.pack(anchor="e")
+texto.pack(anchor="w")
 
 ventana.mainloop()
