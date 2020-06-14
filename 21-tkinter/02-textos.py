74d2090e589127ea96e268d8c189e9e1997b36f2
Javier Piedragil <jpiedragil@uttab.edu.mx>
2020-06-14 17:22:12 -0500

    👌 IMPROVE: Lección 126
    - Cambia altura y ancho de la caja de texto.

--- 21-tkinter/02-textos.py
+++ 21-tkinter/02-textos.py
@@ -9,6 +9,7 @@ texto.config(fg="white", bg="#000000", padx=50, pady=20, font=("Consolas", 30))
 texto.pack()
 
 texto = tk.Label(ventana, text="Soy Javier M. Piedragil")
-texto.pack(anchor="w")
+texto.config(width=400, height=400, bg="orange")
+texto.pack(anchor=tk.SE)
 
 ventana.mainloop()
