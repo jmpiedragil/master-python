c392d3014d0fbcca561d01e702b3327b10a5ad0d
Javier Piedragil <jpiedragil@uttab.edu.mx>
2020-06-14 16:46:43 -0500

    👌 IMPROVE: Lección 126
    - Cambia la definición de color de un texto a valor hexadecimal.
    - Define márgenes interiores del texto.

--- 21-tkinter/02-textos.py
+++ 21-tkinter/02-textos.py
@@ -5,7 +5,7 @@ ventana = tk.Tk()
 ventana.geometry("500x500")
 
 texto = tk.Label(ventana, text="Bienvenido a mi programa")
-texto.config(fg="white", bg="black")
+texto.config(fg="white", bg="#000000", padx=20, pady=20)
 texto.pack()
 
 texto = tk.Label(ventana, text="Soy Javier M. Piedragil")
