7736211a7889a5c9a28217650b57420569cd5533
Javier Piedragil <jpiedragil@uttab.edu.mx>
2020-06-14 20:01:52 -0500

    👌 IMPROVE: LEcción 126
    - Cambia la forma del cursor cuando éste se posiciona por encima
    del texto.

--- 21-tkinter/02-textos.py
+++ 21-tkinter/02-textos.py
@@ -10,7 +10,12 @@ texto.pack()
 
 texto = tk.Label(ventana, text="Soy Javier M. Piedragil")
 texto.config(height=3, bg="orange", font=("Arial", 18), padx=10, pady=20,
-             cursor="circle")
+             cursor="spider")
 texto.pack(anchor=tk.SE)
+
+texto = tk.Label(ventana, text="Master en Python")
+texto.config(height=3, bg="red", font=("Arial", 18), padx=10, pady=20,
+             cursor="spider")
+texto.pack(anchor=tk.NW)
 
 ventana.mainloop()
