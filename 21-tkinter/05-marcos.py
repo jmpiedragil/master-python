df04ea30e695064d171a86102020343e84737eea
Javier Piedragil <jpiedragil@uttab.edu.mx>
2020-06-19 00:10:24 -0500

    👌 IMPROVE:  Lección 130
    - Cambio de algunos parámetros para dar estilo al marco.

--- 21-tkinter/05-marcos.py
+++ 21-tkinter/05-marcos.py
@@ -8,8 +8,8 @@ ventana.geometry("700x400")
 
 marco = tk.Frame(ventana, width=250, height=250)
 
-marco.config(bg="red", bd=12, relief="raised")
-marco.pack()
+marco.config(bg="red", bd=5, relief=tk.RAISED)
+marco.pack(side=tk.RIGHT, anchor=tk.SE)
 
 
 ventana.mainloop()
