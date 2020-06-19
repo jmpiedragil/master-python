109937fbca783ae5f8ee578ec52227be31da99f0
Javier Piedragil <jpiedragil@uttab.edu.mx>
2020-06-19 00:19:14 -0500

    👌 IMPROVE: LEcción 130
    - Crea un marco nuevo.
    - Se cambian las posiciones de los marcos.

--- 21-tkinter/05-marcos.py
+++ 21-tkinter/05-marcos.py
@@ -9,7 +9,12 @@ ventana.geometry("700x400")
 marco = tk.Frame(ventana, width=250, height=250)
 
 marco.config(bg="red", bd=5, relief=tk.RAISED)
-marco.pack(side=tk.LEFT, anchor=tk.NW)
+marco.pack(side=tk.LEFT, anchor=tk.SW)
+
+marco = tk.Frame(ventana, width=250, height=250)
+
+marco.config(bg="green", bd=5, relief=tk.RAISED)
+marco.pack(side=tk.RIGHT, anchor=tk.SE)
 
 
 ventana.mainloop()
